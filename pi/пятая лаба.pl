8af57cb1f9682ff5b19d5347052f4e53e1f9c839
ardipazij <121768780+ardipazij@users.noreply.github.com>
2026-04-18 17:11:26 +0300

    chore(pi.5): Обновление описаний

--- pi/пятая лаба.pl
+++ pi/пятая лаба.pl
@@ -1,6 +1,4 @@
-% Представление графа: edge(Откуда, Куда, Вес)
-% Граф ненаправленный, поэтому описываем ребра в обе стороны или 
-% обрабатываем это в коде.
+% Представление графа: edge(Откуда, Куда, Вес)
 
 graph([
     edge(1, 2, 2), edge(1, 3, 3),
@@ -10,15 +8,13 @@ graph([
     edge(5, 6, 1)
 ]).
 
-% Главный предикат: prim(+StartNode, -MST)
+% Вызов — prim(+StartNode, -MST)
 prim(StartNode, MST) :-
     graph(Edges),
     % Инициализация: U = {StartNode}, P = {}
     solve_prim([StartNode], Edges, [], MST).
-
-% solve_prim(+VisitedNodes, +AllEdges, +CurrentMST, -FinalMST)
-% Условие выхода (Конец): U = V (все вершины посещены).
-% В Prolog проще проверять, нельзя ли добавить новое ребро.
+
+% Условие выхода (Конец): U = V (все вершины посещены).
 solve_prim(Visited, Edges, AccMST, FinalMST) :-
     find_min_edge(Visited, Edges, BestEdge),
     !, % Если нашли ребро, продолжаем
@@ -41,6 +37,6 @@ find_min_edge(Visited, Edges, edge(U, V, W)) :-
     Candidates \= [],
     sort(3, @=<, Candidates, [edge(U, V, W) | _]).
 
-% Вспомогательная функция для обновления списка посещенных вершин
+% Обновление списка посещенных вершин
 update_visited(U, V, Visited, [Next | Visited]) :-
     (member(U, Visited) -> Next = V ; Next = U).
